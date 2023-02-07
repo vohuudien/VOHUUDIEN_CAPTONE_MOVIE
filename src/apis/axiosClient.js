225afbac952cf2e4ad04e928f153d5004c40ef85
LAPTOP-TJTOMUSS\Admin <vohuudien>
2023-02-07 14:44:53 +0700

    prepair token

--- src/apis/axiosClient.js
+++ src/apis/axiosClient.js
@@ -5,7 +5,7 @@ const axiosClient = axios.create({
 	baseURL: "https://movienew.cybersoft.edu.vn/api/",
 	headers: {
 		TokenCybersoft:
-			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyNyIsIkhldEhhblN0cmluZyI6IjIxLzAxLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY3NDI1OTIwMDAwMCIsIm5iZiI6MTY0NjE1NDAwMCwiZXhwIjoxNjc0NDA2ODAwfQ.5JfRY7XxLBvxphVDGE6br_uBYC9AbkqaSEmNTGvzWtQ",
+			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyNyIsIkhldEhhblN0cmluZyI6IjI4LzAzLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY3OTk2MTYwMDAwMCIsIm5iZiI6MTY0NjE1NDAwMCwiZXhwIjoxNjgwMTA5MjAwfQ.2e6OrL1-SuHZKNfdr9iq67HB3GQ_h3v8-evzYyuSIFA",
 	},
 });
 
